6f7d499fe90fb0eb10ac5ca563549b5d6e3dd818
dag2104 <stdhruv2104@gmail.com>
2022-09-02 09:39:27 +0530

    reformatted code

--- src/components/DeleteBlock.js
+++ src/components/DeleteBlock.js
@@ -1,19 +1,21 @@
-import axios from 'axios'
+import axios from "axios";
 
 const DeleteBlock = ({ documentId }) => {
-
   const deleteTicket = async () => {
-    const response = await axios.delete(`http://localhost:8000/tickets/${documentId}`)
-    const success = response.status == 200
-    if (success) window.location.reload()
-  }
-
+    const response = await axios.delete(
+      `http://localhost:8000/tickets/${documentId}`
+    );
+    const success = response.status == 200;
+    if (success) window.location.reload();
+  };
 
   return (
     <div className="delete-block">
-      <div className="delete-icon" onClick={deleteTicket}>✖</div>
+      <div className="delete-icon" onClick={deleteTicket}>
+        ✖
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default DeleteBlock
+export default DeleteBlock;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,24 @@
-import { useNavigate } from 'react-router-dom'
-import logo from '../images/crm-logo.png'
+import { useNavigate } from "react-router-dom";
+import logo from "../images/Web/icons8-completed-task-windows-11-color-96.png";
 
 const Nav = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <nav>
-      <div className="logo-container" onClick={() => navigate('/')}>
-        <img src={logo} alt="logo"/>
+      <div className="logo-container" onClick={() => navigate("/")}>
+        <img src={logo} alt="logo" />
       </div>
       <div className="controls-container">
-        <div className="icon" onClick={() => navigate('/ticket')}>➕</div>
-        <div className="icon" onClick={() => navigate('/')}>❮❮</div>
+        <button className="icon" onClick={() => navigate("/ticket")}>
+          ➕
+        </button>
+        <button className="icon" onClick={() => navigate("/")}>
+          ❮❮
+        </button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
